06fe9a10e47c66cc11ea1350c91f689f6f1ecb59
NidhiPardhi <109077459+NidhiPardhi@users.noreply.github.com>
2023-05-12 19:27:49 +0530

    New

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Nidhi Pardhi
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
